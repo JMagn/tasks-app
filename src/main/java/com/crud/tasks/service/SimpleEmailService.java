0e5d7233d012eff832e82c8ba2f2a53cbc5b8336
JMagn <jakub.magnuski@gmail.com>
2018-10-11 21:22:17 +0200

    Task update

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 
 @Service
 public class SimpleEmailService {
@@ -32,7 +33,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(!mail.getToCc().isEmpty() && mail.getToCc() != null) {
+        if (StringUtils.isNotEmpty(mail.getToCc())) {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
